b08d333f89f971a8ec419cd924cbcfd12575fb0f
henrikvtcodes <commits@henrikvt.com>
2024-07-22 18:14:44 -0400

    enable bird2 (and create readme)

--- hosts/nova/default.nix
+++ hosts/nova/default.nix
@@ -1,34 +1,42 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports = 
-    [
-      ../../modules/common.nix
+  imports = [
+    ../../modules/common.nix
 
-      ../../modules/blocky.nix
-      ../../modules/chrony.nix
-      ../../modules/pathvector.nix
+    ../../modules/blocky.nix
+    ../../modules/chrony.nix
+    ../../modules/pathvector.nix
 
-      ./hardware-configuration.nix
-    ];
+    ./hardware-configuration.nix
+  ];
 
   boot.loader.grub = {
     enable = true;
     device = "/dev/sda";
   };
-  
+
   networking = {
     hostName = "nova";
 
     interfaces.ens18 = {
-      ipv4.addresses = [{
-        address = "192.67.33.7";
-        prefixLength = 26;
-      }];
-      ipv6.addresses = [{
-        address = "2602:fa7e:14::6";
-        prefixLength = 64;
-      }];
+      ipv4.addresses = [
+        {
+          address = "192.67.33.7";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2602:fa7e:14::6";
+          prefixLength = 64;
+        }
+      ];
     };
 
     defaultGateway = {
@@ -39,6 +47,10 @@
       address = "2602:fa7e:14::1";
       interface = "ens18";
     };
+  };
+
+  services.bird2 = {
+    enable = true;
   };
 
   environment.etc."pathvector.yml".source = ./pathvector.yml;
