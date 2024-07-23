f7f190a7459d7d1026c62af35883648a2df0b81e
henrikvtcodes <commits@henrikvt.com>
2024-07-22 21:45:30 -0400

    get rid of pathvector service for now

--- modules/pathvector.nix
+++ modules/pathvector.nix
@@ -13,15 +13,15 @@
     config = builtins.readFile ./bird-default.conf;
   };
 
-  systemd.services.pathvector = {
-    description = "Run pathvector and regenerate on config change";
-    wantedBy = [ "multi-user.target" ];
-    reloadTriggers = [ config.environment.etc."pathvector.yml".source ];
-    requires = [ "bird2.service" ];
-    serviceConfig = {
-      Type = "forking";
-      ExecStart = "${pkgs.pathvector}/bin/pathvector generate";
-      ExecReload = "${pkgs.pathvector}/bin/pathvector generate";
-    };
-  };
+  # systemd.services.pathvector = {
+  #   description = "Run pathvector and regenerate on config change";
+  #   wantedBy = [ "multi-user.target" ];
+  #   reloadTriggers = [ config.environment.etc."pathvector.yml".source ];
+  #   requires = [ "bird2.service" ];
+  #   serviceConfig = {
+  #     Type = "forking";
+  #     ExecStart = "${pkgs.pathvector}/bin/pathvector generate";
+  #     ExecReload = "${pkgs.pathvector}/bin/pathvector generate";
+  #   };
+  # };
 }
