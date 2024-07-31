ad4731ec7acd4e48bebf7b5b1a2fbabb0e24bb0a
tibs <tibs@tibinonest.me>
2024-07-31 12:04:59 -0400

    Let's fix up that blocky config

--- modules/blocky.nix
+++ modules/blocky.nix
@@ -13,19 +13,23 @@
     settings = {
       upstreams.groups.default = [
         "https://ordns.he.net/dns-query"
-        "https://dns.quad9.net/dns-query"
-        "https://security.cloudflare-dns.com/dns-query"
-        "https://base.dns.mullvad.net/dns-query"
-        "https://freedns.controld.com/p2"
-        "https://dns.adguard-dns.com/dns-query"
+        "https://dns10.quad9.net/dns-query"
+        "https://cloudflare-dns.com/dns-query"
+        "https://dns.google/dns-query"
+        "https://dns.mullvad.net/dns-query"
+        "https://freedns.controld.com/p0"
+        "https://unfiltered.adguard-dns.com/dns-query"
+        "https://wikimedia-dns.org/dns-query"
       ];
-      bootstrapDns = {
-        upstream = "https://ordns.he.net/dns-query";
-        ips = [
-          "74.82.42.42"
-          "2001:470:20::2"
-        ];
-      };
+      bootstrapDns = [
+        {
+          upstream = "https://ordns.he.net/dns-query";
+          ips = [
+            "74.82.42.42"
+            "2001:470:20::2"
+          ];
+        }
+      ];
       caching.prefetching = true;
     };
   };
