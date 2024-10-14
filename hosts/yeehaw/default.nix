41cc908751b99e91f9209d3db27c3272a9c46eef
henrikvtcodes <commits@henrikvt.com>
2024-10-14 13:39:35 -0400

    add yeehaw nixos configs

---
+++ hosts/yeehaw/default.nix
@@ -0,0 +1,59 @@
+{ ... }:
+
+{
+  imports = [
+    ../../modules/common.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+
+  networking = {
+    hostName = "strudel";
+
+    interfaces = {
+      # Servperso Systems
+      ens18 = {
+        ipv4.addresses = [
+    {
+      address = "23.150.41.166";
+      prefixLength = 27;
+    }
+  ];
+        ipv6.addresses = [
+    {
+      address = "2602:02b7:40:65::166";
+      prefixLength = 64;
+    }
+  ];
+
+    defaultGateway = {
+    address = "23.150.41.161";
+    interface = "ens18";
+  };
+    defaultGateway6 = {
+    address = "2602:02b7:40:65::1";
+    interface = "ens18";
+  };
+  };
+
+  motd.location = "kansas city, us";
+
+  # See options.pathvector in modules/pathvector.nix
+  services.pathvector = {
+    enable = true;
+    configFile = ./pathvector.yml;
+  };
+
+  metrics.node.enable = true;
+  metrics.node.openFirewall = true;
+  metrics.bird.enable = true;
+  metrics.bird.openFirewall = true;
+  # ======================== DO NOT CHANGE THIS ========================
+  system.stateVersion = "24.05";
+  # ======================== DO NOT CHANGE THIS ========================
+}
