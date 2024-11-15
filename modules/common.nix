50d118af5954cef2df1329772330c5820b9a907d
tibs <tibs@tibinonest.me>
2024-11-15 13:18:46 -0500

    Re-add HE prepends, sysctl options

--- modules/common.nix
+++ modules/common.nix
@@ -54,6 +54,12 @@
     };
   };
 
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding"= 1;
+    "net.ipv6.conf.all.accept_ra" = 0;
+  };
+
   services = {
     iperf3.enable = true;
 
