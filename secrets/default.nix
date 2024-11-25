fb4fb5c06ca84d26085165b62683bdfdb1ef396a
henrikvtcodes <commits@henrikvt.com>
2024-11-25 09:58:16 -0500

    ok then dont include pubkeys??

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,17 +1,17 @@
 { ... }:
 {
   # All machines should be able to access each other's public keys (and admin pubkeys)
-  age.secrets = {
-    bayWgPubkey = ./bayWgPubkey.age;
-    peteWgPubkey = ./peteWgPubkey.age;
-    # novaWgPubkey = ./novaWgPubkey.age;
-    falaiseWgPubkey = ./falaiseWgPubkey.age;
-    mapleWgPubkey = ./mapleWgPubkey.age;
-    strudelWgPubkey = ./strudelWgPubkey.age;
-    tulipWgPubkey = ./tulipWgPubkey.age;
-    yeehawWgPubkey = ./yeehawWgPubkey.age;
+  # age.secrets = {
+  #   bayWgPubkey = ./bayWgPubkey.age;
+  #   peteWgPubkey = ./peteWgPubkey.age;
+  #   # novaWgPubkey = ./novaWgPubkey.age;
+  #   falaiseWgPubkey = ./falaiseWgPubkey.age;
+  #   mapleWgPubkey = ./mapleWgPubkey.age;
+  #   strudelWgPubkey = ./strudelWgPubkey.age;
+  #   tulipWgPubkey = ./tulipWgPubkey.age;
+  #   yeehawWgPubkey = ./yeehawWgPubkey.age;
 
-    henrikWgPubkey = ./henrikWgPubkey.age;
-    # tibsWgPubkey = ./tibsWgPubkey.age;
-  };
+  #   henrikWgPubkey = ./henrikWgPubkey.age;
+  #   # tibsWgPubkey = ./tibsWgPubkey.age;
+  # };
 }
