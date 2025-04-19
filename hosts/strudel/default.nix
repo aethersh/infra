05194c96922dafa0aad8e2bbb7652fd5194e00a9
Henrik VT <commits@henrikvt.com>
2025-04-18 22:43:43 -0400

    wireguard between strudel and tulip

--- hosts/strudel/default.nix
+++ hosts/strudel/default.nix
@@ -95,49 +95,14 @@
       interface = "ens18";
     };
 
+    wireguard.enable = true;
     wireguard.interfaces.wg0 = {
       privateKeyFile = config.age.secrets.strudelWgPrivkey.path;
       ips = [
-        "172.31.0.16/24"
-        "2602:fbcf:dd:de::/48"
+        "172.31.0.10/24"
+        "fd:215:207::1276/56"
       ];
       peers = [
-        {
-          name = "bay";
-          publicKey = "R6XggQejbqHF/l9rvgTJx5a/89Zw0Grd0pJ8OKwkXUE=";
-          endpoint = "bay.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.8/32"
-            "2602:fbcf:dd:d5::/64"
-          ];
-        }
-        {
-          name = "falaise";
-          publicKey = "F80L40pvu3N7pP9EXGS+SbP20NH6z+rHbcOHYBnQwg8=";
-          endpoint = "falaise.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.12/32"
-            "2602:fbcf:dd:d9::/64"
-          ];
-        }
-        {
-          name = "maple";
-          publicKey = "FcSTiNO/GrPeTqfIcHrpOGQks324suM4QcdbGOM1igc=";
-          endpoint = "maple.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.11/32"
-            "2602:fbcf:dd:d8::/64"
-          ];
-        }
-        {
-          name = "pete";
-          publicKey = "UI+GluPRk0biKO+JITUDgOy+4b2LOw7x7TbhML/lC1Q=";
-          endpoint = "pete.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.5/32"
-            "2602:fbcf:dd:d6::/64"
-          ];
-        }
         # {
         #   name = "strudel";
         #   endpoint = "strudel.as215207.net:60908";
@@ -151,19 +116,10 @@
           name = "tulip";
           endpoint = "tulip.as215207.net:60908";
           allowedIPs = [
-            "172.31.0.14/32"
-            "2602:fbcf:dd:db::/64"
+            "172.31.0.20/32"
+            "fd:215:207::528/64"
           ];
           publicKey = "OXdm485MJpI5923eHf5CUqcXjkkUCXkLGqRC4udLMAs=";
-        }
-        {
-          name = "yeehaw";
-          endpoint = "yeehaw.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.7/32"
-            "2602:fbcf:dd:d4::/64"
-          ];
-          publicKey = "BWvxchKyCm2LrXKkVFInjgGWqRVOTyUcKYDGcPpUdR4=";
         }
       ];
     };
--- hosts/tulip/default.nix
+++ hosts/tulip/default.nix
@@ -69,55 +69,20 @@
       interface = "ens18";
     };
 
+    wireguard.enable = true;
     wireguard.interfaces.wg0 = {
       privateKeyFile = config.age.secrets.tulipWgPrivkey.path;
       ips = [
-        "172.31.0.14/24"
-        "2602:fbcf:dd:db::/48"
+        "172.31.0.20/24"
+        "fd:215:207::528/56"
       ];
       peers = [
-        {
-          name = "bay";
-          publicKey = "R6XggQejbqHF/l9rvgTJx5a/89Zw0Grd0pJ8OKwkXUE=";
-          endpoint = "bay.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.8/32"
-            "2602:fbcf:dd:d5::/64"
-          ];
-        }
-        {
-          name = "falaise";
-          publicKey = "F80L40pvu3N7pP9EXGS+SbP20NH6z+rHbcOHYBnQwg8=";
-          endpoint = "falaise.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.1/32"
-            "2602:fbcf:dd:d9::/64"
-          ];
-        }
-        {
-          name = "maple";
-          publicKey = "FcSTiNO/GrPeTqfIcHrpOGQks324suM4QcdbGOM1igc=";
-          endpoint = "maple.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.11/32"
-            "2602:fbcf:dd:d8::/64"
-          ];
-        }
-        {
-          name = "pete";
-          publicKey = "UI+GluPRk0biKO+JITUDgOy+4b2LOw7x7TbhML/lC1Q=";
-          endpoint = "pete.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.5/32"
-            "2602:fbcf:dd:d6::/64"
-          ];
-        }
         {
           name = "strudel";
           endpoint = "strudel.as215207.net:60908";
           allowedIPs = [
-            "172.31.0.16/32"
-            "2602:fbcf:dd:de::/64"
+            "172.31.0.10/32"
+            "fd:215:207::1276/64"
           ];
           publicKey = "Q/uu+7+gMFT57LkFH4tu1bMrfQSuYiR/OALmR/oJaBY=";
         }
@@ -130,15 +95,6 @@
         #   ];
         #   publicKey = "OXdm485MJpI5923eHf5CUqcXjkkUCXkLGqRC4udLMAs=";
         # }
-        {
-          name = "yeehaw";
-          endpoint = "yeehaw.as215207.net:60908";
-          allowedIPs = [
-            "172.31.0.7/32"
-            "2602:fbcf:dd:d4::/64"
-          ];
-          publicKey = "BWvxchKyCm2LrXKkVFInjgGWqRVOTyUcKYDGcPpUdR4=";
-        }
       ];
     };
   };
