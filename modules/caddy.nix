a9f67f07e14e9e7a4c036e0b38782dd12e6c1374
Henrik VT <commits@henrikvt.com>
2025-04-22 14:16:33 -0400

    try caddy build with unstable

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  unstable,
   lib,
   config,
   ...
@@ -7,12 +8,12 @@
 {
   services.caddy = {
     enable = true;
-    # package = (
-    #   pkgs.caddy.withPlugins {
-    #     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-    #     hash = lib.fakeHash;
-    #   }
-    # );
+    package = (
+      unstable.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
+        hash = lib.fakeHash;
+      }
+    );
     virtualHosts."anycast.as215207.net" = {
       extraConfig = ''
         respond "You have reached the anycast IPv6 address of AS215207: 2602:fbcf:df::1 \n This is being served by ${config.networking.hostName} in ${config.motd.location} \n Ping this server directly at 6.${config.networking.hostName}.as215207.net"
