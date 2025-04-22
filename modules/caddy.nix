49cfd03c865a420fc969ba52530a0dc132ff6c2d
Henrik VT <commits@henrikvt.com>
2025-04-22 13:33:41 -0400

    comment out caddy.withPlugins for now

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -7,12 +7,12 @@
 {
   services.caddy = {
     enable = true;
-    package = (
-      pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-        hash = lib.fakeHash;
-      }
-    );
+    # package = (
+    #   pkgs.caddy.withPlugins {
+    #     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
+    #     hash = lib.fakeHash;
+    #   }
+    # );
     virtualHosts."anycast.as215207.net" = {
       extraConfig = ''
         respond "You have reached the anycast IPv6 address of AS215207: 2602:fbcf:df::1 \n This is being served by ${config.networking.hostName} in ${config.motd.location} \n Ping this server directly at 6.${config.networking.hostName}.as215207.net"
