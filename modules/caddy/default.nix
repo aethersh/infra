5828d6d243f26ae1b406b6ba41414e8f32e562fc
Henrik VT <commits@henrikvt.com>
2025-04-29 11:19:23 -0400

    move caddy to optional thing

--- modules/caddy/default.nix
+++ modules/caddy/default.nix
@@ -3,65 +3,62 @@
   unstable,
   lib,
   config,
+  inputs,
   ...
 }:
-let
-  caddyWithCfDns = unstable.caddy.withPlugins {
-    plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-    hash = "sha256-UwrkarDwfb6u+WGwkAq+8c+nbsFt7sVdxVAV9av0DLo=";
-  };
-in
 {
-  age.secrets.caddyEnvVars = {
-    file = ../../secrets/caddyEnvVars.age;
-    owner = "caddy";
-    group = "caddy";
-  };
-  # system.activationScripts."caddy-secrets" = ''
-  #   cloudflare_tls_api_key=$(cat "${config.age.secrets.cfDnsKey.path}")
-  #   configFile=${config.services.caddy.configFile}
-  #   ${pkgs.gnused}/bin/sed -i "s/@cloudflare_tls_api_key@/$cloudflare_tls_api_key/" "$configFile"
-  # '';
-
-  services.caddy = {
-    enable = true;
-    package = caddyWithCfDns;
-    extraConfig = builtins.readFile ./Caddyfile;
-    logFormat = lib.mkForce ''
-      level INFO
-    '';
-    virtualHosts = {
-      "anycast.as215207.net" = {
-        extraConfig = ''
-          import cf-dns-v6
-          respond "You have reached the anycast IPv6 address of AS215207: 2602:fbcf:df::1 . This is being served by ${config.networking.hostName}.as215207.net in ${config.motd.location}. Ping this server directly at 6.${config.networking.hostName}.as215207.net"
-        '';
-      };
-      "${config.networking.hostName}.as215207.net" = {
-        extraConfig = ''
-          import cf-dns-v6
-          respond "You have reached the service address of ${config.networking.hostName}.as215207.net in ${config.motd.location}. Try the AS215207 address at 6.${config.networking.hostName}.as215207.net"
-        '';
-      };
-      "6.${config.networking.hostName}.as215207.net" = {
-        extraConfig = ''
-          import cf-dns-v6
-          respond "You have reached the AS215207 IPv6 address of ${config.networking.hostName}.as215207.net in ${config.motd.location}"
-        '';
-      };
+  options.ae.caddy = with lib; {
+    enable = mkEnableOption {
+      default = false;
+      description = "Enable Caddy";
     };
   };
 
-  # systemd.services.caddy.serviceConfig.EnviromentFile = config.age.secrets.caddyEnvVars.path;
+  config = lib.mkIf config.ae.caddy.enable {
+    age.secrets.caddyEnvVars = {
+      file = ../../secrets/caddyEnvVars.age;
+      owner = "caddy";
+      group = "caddy";
+    };
 
-  # List that starts with an empty string forces systemd to "reset" instead of appending a second ExecStart value. This way, we replace the existing one
-  systemd.services.caddy.serviceConfig.ExecStart = lib.mkForce [
-    ""
-    "${caddyWithCfDns}/bin/caddy run --config /etc/caddy/caddy_config --adapter caddyfile --envfile ${config.age.secrets.caddyEnvVars.path}"
-  ];
+    services.caddy = {
+      enable = true;
+      package = inputs.outputs.packages.caddy;
+      extraConfig = builtins.readFile ./Caddyfile;
+      logFormat = lib.mkForce ''
+        level INFO
+      '';
+      virtualHosts = {
+        "anycast.as215207.net" = {
+          extraConfig = ''
+            import cf-dns-v6
+            respond "You have reached the anycast IPv6 address of AS215207: 2602:fbcf:df::1 . This is being served by ${config.networking.hostName}.as215207.net in ${config.motd.location}. Ping this server directly at 6.${config.networking.hostName}.as215207.net"
+          '';
+        };
+        "${config.networking.hostName}.as215207.net" = {
+          extraConfig = ''
+            import cf-dns-v6
+            respond "You have reached the service address of ${config.networking.hostName}.as215207.net in ${config.motd.location}. Try the AS215207 address at 6.${config.networking.hostName}.as215207.net"
+          '';
+        };
+        "6.${config.networking.hostName}.as215207.net" = {
+          extraConfig = ''
+            import cf-dns-v6
+            respond "You have reached the AS215207 IPv6 address of ${config.networking.hostName}.as215207.net in ${config.motd.location}"
+          '';
+        };
+      };
+    };
+    # List that starts with an empty string forces systemd to "reset" instead of appending a second ExecStart value. This way, we replace the existing one
+    systemd.services.caddy.serviceConfig.ExecStart = lib.mkForce [
+      ""
+      "${config.services.caddy.package}/bin/caddy run --config /etc/caddy/caddy_config --adapter caddyfile --envfile ${config.age.secrets.caddyEnvVars.path}"
+    ];
 
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-  ];
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+
 }
