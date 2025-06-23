ce1683b66be2a37b2769a21540b4d1540ac2e115
Henrik VT <commits@henrikvt.com>
2025-06-23 13:58:44 -0400

    fix deployrs thing

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
       deployPkgs = import nixpkgs {
         inherit system;
         overlays = [
-          deploy-rs.overlay
+          deploy-rs.overlays.default
           (self: super: {
             deploy-rs = {
               inherit (pkgs) deploy-rs;
